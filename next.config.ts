9528aea1294dd7ac3245be11c87147b49b8c15f8
Likhit Packers <likhitpg08092020@gmail.com>
2026-09-09 09:21:08 +0530

    Fix next.config.ts for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/likhitpackers',
+  assetPrefix: '/likhitpackers',
   images: {
     unoptimized: true,
   },
